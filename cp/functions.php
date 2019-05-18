7bd20c6b81714a537e9044e1401076a12cb87834
netristech <32661141+netristech@users.noreply.github.com>
2019-05-17 22:39:46 -0500

    Update functions.php

--- cp/functions.php
+++ cp/functions.php
@@ -18,8 +18,17 @@ if(isset($_POST['action']) && !empty($_POST['action'])) {
                 case 'disable-wifi':
                         disableWifi();
                         break;
+                case 'fix':
+                        autoFix();
+                        break;
                 case 'update':
                         updateCode();
+                        break;
+                case 'list-backups':
+                        listBackups();
+                        break;                        
+                case 'restore':
+                        restoreBackup();
                         break;
         }
 }
@@ -34,8 +43,8 @@ function initFunc() {
         echo "<pre>" . $output . "</pre>";
 }
 
-function updateCode() {
-        $output = shell_exec("sudo ./update.sh");
+function restartVM() {
+        $output = shell_exec("sudo ./restart.sh");
         echo "<pre>" . $output . "</pre>";
 }
 
@@ -44,13 +53,28 @@ function shutdownVM() {
         echo "<pre>" . $output . "</pre>";
 }
 
-function restartVM() {
-        $output = shell_exec("sudo ./restart.sh");
+function disableWifi() {
+        $output = shell_exec("sudo ./disable-wifi.sh");
         echo "<pre>" . $output . "</pre>";
 }
 
-function disableWifi() {
-        $output = shell_exec("sudo ./disable-wifi.sh");
+function autoFix() {
+        $output = shell_exec("sudo ./fix.sh");
+        echo "<pre>" . $output . "</pre>";
+}
+
+function updateCode() {
+        $output = shell_exec("sudo ./update.sh");
+        echo "<pre>" . $output . "</pre>";
+}
+
+function listBackups() {
+        $output = shell_exec("sudo ./list.sh");
+        echo "<pre>" . $output . "</pre>";
+}
+
+function restoreBackup() {
+        $output = shell_exec("sudo ./restore.sh");
         echo "<pre>" . $output . "</pre>";
 }
 
