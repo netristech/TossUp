2413df8da714362603e90442fbb0369e2542a463
netristech <32661141+netristech@users.noreply.github.com>
2019-05-20 14:22:37 -0500

    Update fix.sh

--- cp/fix.sh
+++ cp/fix.sh
@@ -1,6 +1,11 @@
 echo "Restarting services... This may take a few minutes. Please be patient. . . "
 /etc/init.d/network restart
+ifdown enp0s3
 ifdown enp0s8
+rm -f /etc/sysconfig/network-scripts/ifcfg-enp0s3
+cp /root/ifcfg-enp0s3 /etc/sysconfig/network-scripts/
+chmod 500 /etc/sysconfig/network-scripts/ifcfg-enp0s3
+ifup enp0s3
 ifup enp0s8
 systemctl stop httpd
 systemctl start httpd
