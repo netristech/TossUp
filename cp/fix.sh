073eae8eb2638308ba8afbe13a3952b79ac2a21d
netristech <32661141+netristech@users.noreply.github.com>
2019-05-20 14:05:48 -0500

    Update fix.sh

--- cp/fix.sh
+++ cp/fix.sh
@@ -1,8 +1,4 @@
 echo "Restarting services... This may take a few minutes. Please be patient. . . "
-sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 200M/g' /etc/php.ini
-sed -i 's/post_max_size = 8/post_max_size = 200/g' /etc/php.ini
-sed -i 's/max_execution_time = 30/max_execution_time = 120/g' /etc/php.ini
-sed -i 's/max_input_time = 60/max_input_time = 240/g' /etc/php.ini
 /etc/init.d/network restart
 ifdown enp0s8
 ifup enp0s8
