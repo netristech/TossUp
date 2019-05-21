480277d35bbda05d2f99a981c0438a237a6b906c
netristech <32661141+netristech@users.noreply.github.com>
2019-05-21 14:48:57 -0500

    Update update.sh

--- cp/update.sh
+++ cp/update.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 #update TossUp app from GitHub
 
-echo "|----------------------------------------------|"
-echo "| Starting update of TossUp app. This may take |"
-echo "| a few minutes. Please be patient...          |"
-echo "|----------------------------------------------|"
+echo "Now starting update of TossUp from GitHub."
+echo "This can take a few minutes. Please be patient. . ."          |"
 echo " "
 systemctl stop httpd
 ifdown enp0s3
 tar -czf /root/backup/backup_$(date +"%Y%m%d").tgz -C /var/www html
 cd /var/www/html
-#git pull origin master
 git fetch --all
 git reset --hard origin/master
 chown -R apache:apache /var/www/html
 cd /root
 ifup enp0s3
 systemctl start httpd
-echo " "
 echo "Update process complete!"
-echo " "
