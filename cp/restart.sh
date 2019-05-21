e20785ff3ee351a43f014f8e6b7356f85f5e0ad3
netristech <32661141+netristech@users.noreply.github.com>
2019-05-21 14:43:14 -0500

    Update restart.sh

--- cp/restart.sh
+++ cp/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Please wait while the TossUp environment is restarted. . ."
+echo "The TossUp environment is going down for reboot. Please wait a minute and then refresh your screen."
 echo ""
 shutdown --reboot now
