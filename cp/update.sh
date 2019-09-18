a7a48a8c05cfc1d4bfd10d489d0c1cbd838d326d
netristech <32661141+netristech@users.noreply.github.com>
2019-09-18 11:11:40 -0400

    Update update.sh

--- cp/update.sh
+++ cp/update.sh
@@ -2,13 +2,13 @@
 #update TossUp app from GitHub
 
 echo "Now updating. . ."
-ifdown enp0s3
+#ifdown enp0s3
 tar -czf /root/backup/backup_$(date +"%Y%m%d").tgz -C /var/www html
 cd /var/www/html
 git fetch --all
 git reset --hard origin/master
 chown -R apache:apache /var/www/html
 cd /root
-ifup enp0s3
+#ifup enp0s3
 systemctl reload httpd
 echo "Update completed successfully!"
