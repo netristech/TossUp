826e9d5233beeda540b1eba82fc69ccaf3cd015b
netristech <32661141+netristech@users.noreply.github.com>
2019-09-22 23:12:35 -0400

    Update update.sh

--- cp/update.sh
+++ cp/update.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 #update TossUp app from GitHub
 
-echo "Now updating. . ."
-#ifdown enp0s3
+#Update TossUp Application
+git config --global credential.helper store
+if [ ! -z "$username"] && [ ! -z "$password"]; then
+  username=$1
+  password=$2
+  echo "https://$username:$password@github.com" > /root/.git-credentials
+fi
 tar -czf /root/backup/backup_$(date +"%Y%m%d").tgz -C /var/www html
 cd /var/www/html
 git fetch --all
 git reset --hard origin/master
+
+#Update control panel
+cd /root/ && git pull origin/master
+command cp -r /root/cp/* /var/www/html/cp/
+
 chown -R apache:apache /var/www/html
-cd /root
-#ifup enp0s3
 systemctl reload httpd
-echo "Update completed successfully!"
+echo "Update completed!"
