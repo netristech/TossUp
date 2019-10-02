99be3b8913daac0ced87bb45c68d4a56974c127a
netristech <32661141+netristech@users.noreply.github.com>
2019-10-01 20:28:31 -0500

    Update update.sh

--- cp/update.sh
+++ cp/update.sh
@@ -2,6 +2,7 @@
 #update TossUp app from GitHub
 
 #Update TossUp Application
+echo "Updating TossUp App"
 git config --global credential.helper store
 if [ ! -z "$username"] && [ ! -z "$password"]; then
   username=$1
@@ -14,6 +15,7 @@ git fetch --all
 git reset --hard origin/master
 
 #Update control panel
+echo "Updating Control Panel"
 cd /root/ && git pull origin master
 command cp -r /root/cp/* /var/www/html/cp/
 chmod 550 /var/www/html/cp/*.sh
