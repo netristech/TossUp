37b7260fd28b35cacd3f138a26c5db95040c6ce5
netristech <32661141+netristech@users.noreply.github.com>
2019-10-13 21:54:08 -0400

    Update fix.sh

--- cp/fix.sh
+++ cp/fix.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-ifdown enp0s8
+#ifdown enp0s8
 #ifdown enp0s3
+if [ $(cat /etc/default/grub | grep -c 'GRUB_CMDLINE_LINUX="ipv6.disable=1') -eq 0 ]; then
+  sed -i 's/\<GRUB_CMDLINE_LINUX="\>/&ipv6.disable=1/' /etc/default/grub
+  grub2-mkconfig -o /boot/grub2/grub.cfg
+  echo "Repairs have been made that require reboot. Please use the 'Restart VM' button after completion"
+fi
 command cp /root/ifcfg-enp0s3 /etc/sysconfig/network-scripts/
 chmod 500 /etc/sysconfig/network-scripts/ifcfg-enp0s3
-#ifup enp0s3
+ifup enp0s3
 ifup enp0s8
 systemctl reload httpd
 ip addr show | grep "enp0s3"
